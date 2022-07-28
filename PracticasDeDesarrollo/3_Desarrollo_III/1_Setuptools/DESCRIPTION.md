103ecd561b3b314916d4aadb3204a5964c40e1a7
MG <mtsgrinberg@gmail.com>
2022-07-28 17:09:13 -0300

    clase 3 fixes

--- PracticasDeDesarrollo/3_Desarrollo_III/1_Setuptools/DESCRIPTION.md
+++
@@ -1,3 +0,0 @@
-# Humai Utils
-
-Este es un paquete con muchas funciones comunes que encontramos en nuestros proyectos y que queremos compartir con la comunidad.
--- PracticasDeDesarrollo/3_Desarrollo_III/1_Setuptools/functions.py
+++
@@ -1,8 +0,0 @@
-# No usamos este paquete pero igual lo importamos para generar una dependencia.
-import requests
-
-def saludar_comunidad():
-	print('Hola comunidad de Humai')
-
-def informar_clase():
-	print('La siguiente clase es el lunes')
